f1522156e31e91541b448a7afeb2ba5ad060fb4f
Leonardo Graterol <pankas87@gmail.com>
2017-01-17 14:36:28 -0300

    Fixes

--- Problem-5/run.py
+++ Problem-5/run.py
@@ -2,9 +2,10 @@ from functions import *
 
 # Numbers smaller than 10 that are factors of the numbers larger than 10 are removed
   # A number that is divisible by 20 will be divisible by 2 and 4 and 5
+  # A number that is divisible by 18 will be divisible by 3 and 6
   # A number that is divisible by 15 will be divisible by 3 and 5
   # With less numbers to divide, the calculations will take less time
-numbers = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+numbers = [7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 i       = 20
 
 while not is_divisible_by_list(i, numbers):
